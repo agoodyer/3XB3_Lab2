9fd3d670598f26c31315cc40533ebf046e146dab
Aidan <goodyera@mcmaster.ca>
2023-10-23 09:56:43 -0400

    added approx1

--- Lab2/vertex_cover.py
+++ Lab2/vertex_cover.py
@@ -2,9 +2,14 @@ from graph import *
 from experiments2 import generate_random_graph
 
 
-def check_vertex_cover(G, C):
-    pass 
 
+       
+def copy(G):
+    graph = Graph(G.number_of_nodes())
+    for i in range(G.number_of_nodes()):
+        graph.adj[i] = G.adj[i].copy()
+
+    return graph 
 
 
 
@@ -19,19 +24,28 @@ def remove_incident(G, v):
 
 
 def approx1(G):
-    C = set()
-    max_degree, max_vertex = 0,0
-    for i in range(G.number_of_nodes()):
-        if(len(G.adjacent_nodes(i)) > max_degree):
-            max_degree, max_vertex = len(G.adjacent_nodes(i)), i
-    
-    C.add(max_vertex)
-    remove_incident(G, max_vertex)
 
+    graph = copy(G)
+    C = set()
+
+    while(not is_vertex_cover(graph,C)):
+        max_degree, max_vertex = 0,0
+        for i in range(graph.number_of_nodes()):
+            if(len(graph.adjacent_nodes(i)) > max_degree):
+                max_degree, max_vertex = len(graph.adjacent_nodes(i)), i
+        
+        C.add(max_vertex)
+        remove_incident(graph, max_vertex)
+
+    return C
+
+    
 
 g = generate_random_graph(5, 8)
+print(approx1(g))
 
-approx1(g)
+
+
 
 
 graph = Graph(7)
@@ -42,3 +56,6 @@ graph.add_edge(3,4)
 graph.add_edge(3,5)
 graph.add_edge(4,5)
 graph.add_edge(4,6)
+
+
+
